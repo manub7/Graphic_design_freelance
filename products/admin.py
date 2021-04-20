da348349ca78a360b9b4774cdb5cb6a847e1cfd6
Emanuel Teodor Blaga <manub7@yahoo.com>
2021-04-20 18:35:10 +0000

    Configured productAdmin and categoryAdmin and setup the prodcuts.html page

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,23 @@ from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'size',
+        'price',
+        'image',
+    )
+
+    ordering = ('category',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
