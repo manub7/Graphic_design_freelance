cf4467be5a5b02a9062704158bf41f646cc600b4
Emanuel Teodor Blaga <manub7@yahoo.com>
2021-04-30 19:13:10 +0000

    Added products page  and set up the carousel on the home page

--- home/views.py
+++ home/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from products.models import Product
 # Create your views here.
 def index(request):
-    """ A view to return the index page  """
-    return render(request, 'home/index.html')
+    """ A view to return the index page and first 8 products  """
+    products = Product.objects.all()
+    
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'home/index.html', context)
