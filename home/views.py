9498309e0df5231b24a5a9119bf0e58a540c7ad3
Emanuel Teodor Blaga <manub7@yahoo.com>
2021-05-07 16:01:28 +0000

    Added product detail functionality

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from products.models import Product
+
+
 # Create your views here.
 def index(request):
     """ A view to return the index page and first 8 products  """
@@ -8,5 +10,4 @@ def index(request):
     context = {
         'products': products,
     }
-
     return render(request, 'home/index.html', context)
