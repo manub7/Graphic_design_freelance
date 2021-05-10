f23534ea01dce48dd94ada1c83eafd9858b34348
Emanuel Teodor Blaga <manub7@yahoo.com>
2021-05-10 21:59:33 +0000

    Added design_request views and price calculation logic

--- design_requests/views.py
+++ design_requests/views.py
@@ -5,6 +5,26 @@ from django.conf import settings
 # Create your views here.
 def design_requests(request):
     
-    context = {}
+     price = 0
+     width = 0 
+     height = 0 
+     size= 0 
+    
+     if 'width' in request.POST:
+         width = request.POST['width']
+     if 'height' in request.POST:
+         height = request.POST['height']
+     
+     size = width*height/1024
+
+     price = size * settings.PRICE_FACTOR
+
+    context = {
+        'price': price,
+        'width': width,
+        'height': height,
+        'size':size,
+        'price_factor':settings.PRICE_FACTOR,
+    }
     
     return render(request, 'design_requests/design_requests.html',  )
