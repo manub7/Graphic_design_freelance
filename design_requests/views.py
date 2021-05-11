bef6a1d8b86144fcba6686055351f3652765cc48
Emanuel Teodor Blaga <manub7@yahoo.com>
2021-05-11 09:04:26 +0000

    Added size and price calc functionality in backend with Python and Jquery price calc functionality for frontend

--- design_requests/views.py
+++ design_requests/views.py
@@ -1,23 +1,29 @@
 from django.shortcuts import render
 from decimal import Decimal
 from django.conf import settings
+from products.models import Category, Product
 
 # Create your views here.
 def design_requests(request):
     
-     price = 0
-     width = 0 
-     height = 0 
-     size= 0 
+    price = 0
+    width = 0 
+    height = 0 
+    size= 0 
     
-     if 'width' in request.POST:
+    if 'width' in request.POST:
          width = request.POST['width']
-     if 'height' in request.POST:
+         print(width)
+    if 'height' in request.POST:
          height = request.POST['height']
+         print(height)
      
-     size = width*height/1024
+    size = int(width) * int(height)*3/1024
+    print(size)
 
-     price = size * settings.PRICE_FACTOR
+    price = size * settings.PRICE_FACTOR
+    print(price)
+    categories = Category.objects.all()
 
     context = {
         'price': price,
@@ -25,6 +31,7 @@ def design_requests(request):
         'height': height,
         'size':size,
         'price_factor':settings.PRICE_FACTOR,
+        'categories': categories,
     }
     
-    return render(request, 'design_requests/design_requests.html',  )
+    return render(request, 'design_requests/design_requests.html', context )
