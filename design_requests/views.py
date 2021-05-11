3b3750f8b661db1e4ab50f186094dc315d01ec68
Emanuel Teodor Blaga <manub7@yahoo.com>
2021-05-11 11:42:13 +0000

    Fixed the jquery on the request design page and moved al js in the include folder

--- design_requests/views.py
+++ design_requests/views.py
@@ -3,7 +3,8 @@ from decimal import Decimal
 from django.conf import settings
 from products.models import Category, Product
 
-# Create your views here.
+# A view to create, calculate and capture a design request
+
 def design_requests(request):
     
     price = 0
@@ -13,16 +14,11 @@ def design_requests(request):
     
     if 'width' in request.POST:
          width = request.POST['width']
-         print(width)
     if 'height' in request.POST:
          height = request.POST['height']
-         print(height)
      
     size = int(width) * int(height)*3/1024
-    print(size)
-
-    price = size * settings.PRICE_FACTOR
-    print(price)
+    price = format(size * settings.PRICE_FACTOR,".2f")
     categories = Category.objects.all()
 
     context = {
