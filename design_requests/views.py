9b531d967b4b8c89386fb1795d17c4cb5cb4cee6
Emanuel Teodor Blaga <manub7@yahoo.com>
2021-05-12 10:54:55 +0000

    Fixing merging issues

--- design_requests/views.py
+++ design_requests/views.py
@@ -3,22 +3,12 @@ from decimal import Decimal
 from django.conf import settings
 from products.models import Category, Product
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-# Create your views here.
 
 # A view to create, calculate and capture a design request
 
 
-# Create your views here.
 
-=======
-# Create your views here.
->>>>>>> parent of 3b3750f ( Fixed the jquery on the request design page and moved al js in the include folder)
-=======
-# Create your views here.
->>>>>>> parent of 3b3750f ( Fixed the jquery on the request design page and moved al js in the include folder)
 def design_requests(request):
     
     price = 0
@@ -28,13 +18,9 @@ def design_requests(request):
     
     if 'width' in request.POST:
          width = request.POST['width']
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
->>>>>>> parent of 3b3750f ( Fixed the jquery on the request design page and moved al js in the include folder)
-=======
->>>>>>> parent of 3b3750f ( Fixed the jquery on the request design page and moved al js in the include folder)
+
+
          print(width)
     if 'height' in request.POST:
          height = request.POST['height']
@@ -45,8 +31,6 @@ def design_requests(request):
 
     price = size * settings.PRICE_FACTOR
     print(price)
-<<<<<<< HEAD
-<<<<<<< HEAD
 
          print(width)
 
@@ -64,10 +48,7 @@ def design_requests(request):
     price = size * settings.PRICE_FACTOR
     print(price)
 
-=======
->>>>>>> parent of 3b3750f ( Fixed the jquery on the request design page and moved al js in the include folder)
-=======
->>>>>>> parent of 3b3750f ( Fixed the jquery on the request design page and moved al js in the include folder)
+
     categories = Category.objects.all()
 
     context = {
