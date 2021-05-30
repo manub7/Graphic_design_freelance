c4ba5699aabc3fdc092d92f5852b22ef0d1e1faf
Emanuel Teodor Blaga <manub7@yahoo.com>
2021-05-30 14:42:21 +0000

    Fixed if statement in settings file

--- graphic_design_freelance/settings.py
+++ graphic_design_freelance/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = IF 'DEVELOPMENT' in os.environ
+DEBUG = if 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['manub-graphic-design-freelance.herokuapp.com', 'localhost']
 
